979b93d3aca84004e8681692339a9ff9ff14acfe
HusseinMAbushawish <hussen2001108@gmail.com>
2025-07-12 16:31:21 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AddcaseController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactControoler;
 use App\Http\Controllers\OperationController;
-use App\Http\Controllers\ProfileController ;
+use App\Http\Controllers\ProfileController;
 use Faker\Guesser\Name;
 // use App\Http\Controllers\User\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -26,23 +26,21 @@ Route::get('/', function () {
     return view('index');
 });
 
- 
 
-Route::get('profile-edit',[ProfileController::class,'update'])->middleware('auth');
-Route::get('profile',[ProfileController::class,'index'])->middleware('auth');
+
+Route::get('profile-edit', [ProfileController::class, 'update'])->middleware('auth');
+Route::get('profile', [ProfileController::class, 'index'])->middleware('auth');
 // Route::view('/profile/edit','html.edit-profile')->middleware('auth');
 
 
-Route::resource('addcase',AddcaseController::class)->middleware('auth');
+Route::resource('addcase', AddcaseController::class)->middleware('auth');
 
-Route::get('contact',[ContactControoler::class,'index'])->name('contact.index')->middleware('auth');
-Route::post('contact',[ContactControoler::class,'submit'])->name('contact.submit')->middleware('auth');
-
-
-Route::resource('operation',OperationController::class)->middleware('auth');
-
-Route::view('ddd','html.patient')->middleware('auth');
-
-Route::resource('/home',BlogController::class)->middleware('auth');
+Route::get('contact', [ContactControoler::class, 'index'])->name('contact.index')->middleware('auth');
+Route::post('contact', [ContactControoler::class, 'submit'])->name('contact.submit')->middleware('auth');
 
 
+Route::resource('operation', OperationController::class)->middleware('auth');
+
+Route::view('ddd', 'html.patient')->middleware('auth');
+
+Route::resource('/home', BlogController::class)->middleware('auth');
